688266c40616f9618f96753823abff31dfe4e8f7
piyushsachdeva <piyush.sachdeva055@gmail.com>
2023-05-13 09:29:41 -0400

    pre-req

---
+++ pre-req.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RESOURCE_GROUP_NAME=backend-rg
+STORAGE_ACCOUNT_NAME=backendsa101
+CONTAINER_NAME=tfstate
+
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location canadacentral
+
+# Create storage account
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
+# Create blob container
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
